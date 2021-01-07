80337cb1b7ee92d895204cb1b58c74f4ac794410
dunamisyoung <pksd5693@gmail.com>
2021-01-08 00:53:48 +0900

    Feat : Sass를 통한 button 스타일링

--- src/SassTotorial.js
+++ src/SassTotorial.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import Button from './components/Button';
+import './SassTotorial.scss';
 
 const SassTotorial = () => {
   return (
-    <div>
+    <div className="SassTotorial">
       <div className="buttons">
+        <Button size="large">BUTTON</Button>
         <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button color="gray" size="large">
+          BUTTON
+        </Button>
+        <Button color="gray">BUTTON</Button>
+        <Button color="gray" size="small">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button color="pink" size="large">
+          BUTTON
+        </Button>
+        <Button color="pink">BUTTON</Button>
+        <Button color="pink" size="small">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" outline>
+          BUTTON
+        </Button>
+        <Button color="gray" outline>
+          BUTTON
+        </Button>
+        <Button color="pink" outline size="small">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth>
+          BUTTON
+        </Button>
+        <Button color="gray" size="large" fullWidth>
+          BUTTON
+        </Button>
+        <Button color="pink" size="large" fullWidth>
+          BUTTON
+        </Button>
       </div>
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import classNames from 'classnames';
+
 import './Button.scss';
 
-const Button = ({ children }) => {
-  return <button className="Button">{children}</button>;
+// large, medium, small
+const Button = ({ children, size, color, outline, fullWidth }) => {
+  return <button className={classNames('Button', size, color, { outline, fullWidth })}>{children}</button>;
+};
+
+Button.defaultProps = {
+  size: 'medium',
+  color: 'blue',
 };
 
 export default Button;
