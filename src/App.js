3e4dd02757a8ab17be9fa1dc0ee9553a9926d026
dunamisyoung <pksd5693@gmail.com>
2021-01-09 21:16:44 +0900

    Feat : 서브라우터 활용 실습

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 const App = () => {
   return (
@@ -15,12 +15,15 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
       </ul>
       <hr />
       {/* Route path="주소규칙" component={보여줄 컴포넌트}*/}
       <Route path="/" exact component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/profiles/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
       {/* profiles 뒤에있는 :username 과 같이 생긴게 url 파라미터이다. */}
     </div>
   );
