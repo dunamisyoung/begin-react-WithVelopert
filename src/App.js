2f1b18ccd49c72114180b03b5bcf1a263f7d38ef
dunamisyoung <pksd5693@gmail.com>
2021-01-09 20:41:48 +0900

    Feat : Link와 Route 기본사용법

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+
 const App = () => {
-  return <div>안녕</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          {/* a태그를 대체 사용법은 Link to="주소" */}
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      {/* Route path="주소규칙" component={보여줄 컴포넌트}*/}
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+    </div>
+  );
 };
 
 export default App;
