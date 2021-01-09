2c0dc694491997c39898dadd68808e7880c661cc
dunamisyoung <pksd5693@gmail.com>
2021-01-09 20:28:56 +0900

    Feat : reactRouter 실습 및 초기셋팅

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
-import TodoCreate from './components/TodoCreate';
-import TodoHead from './components/TodoHead';
-import TodoList from './components/TodoList';
-import TodoTemplate from './components/TodoTemplate';
-import { TodoProvider } from './TodoContext';
-
-const GlobalStyle = createGlobalStyle`
-body {
-  background :#e9ecef;
-}
-`;
-
 const App = () => {
-  return (
-    <TodoProvider>
-      <GlobalStyle />
-      <TodoTemplate>
-        <TodoHead />
-        <TodoList />
-        <TodoCreate />
-      </TodoTemplate>
-    </TodoProvider>
-  );
+  return <div>안녕</div>;
 };
 
 export default App;
