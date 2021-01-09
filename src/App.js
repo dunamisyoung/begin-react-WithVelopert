a9268a5b2f020934fc00e75a985ade232767235d
dunamisyoung <pksd5693@gmail.com>
2021-01-09 22:05:15 +0900

    Feat : Switch 사용과 NotFound 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import About from './About';
 import HistorySample from './HistorySample';
 import Home from './Home';
@@ -25,10 +25,23 @@ const App = () => {
       </ul>
       <hr />
       {/* Route path="주소규칙" component={보여줄 컴포넌트}*/}
-      <Route path="/" exact component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/history" component={HistorySample} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+          /* 
+          Switch는 가장 먼저 매칭되는 단 하나의 페이지를 보여준다.
+          주로 404 페이지를 만들때 path 없이 Route render={ ... } 형식으로한다. */
+        />
+      </Switch>
       {/* profiles 뒤에있는 :username 과 같이 생긴게 url 파라미터이다. */}
     </div>
   );
